9fd2a6fed79d7a70151257573654c34cec6043ce
rogerramosruiz <rogerramosruizdev@gmail.com>
2022-04-01 22:49:35 -0400

    bug of no appearing objects resolved

--- ground.py
+++ ground.py
@@ -1,11 +1,17 @@
 import bpy
 from camera import projectCam, changeFocalLength
 def adjustGround():
+    # Adjust the panel for the ground
     ground = bpy.context.scene.objects['Ground']
     coords = projectCam()
-    # Adjust the palen
-    if len(coords) < 4:
+    sw = False
+    for i in coords:
+        if abs(i[0]) >= 100 or abs(i[1]) >= 100 or abs(i[2]) >= 100:
+            sw = True
+            break
+    if len(coords) < 4 or sw:
         changeFocalLength(50)
         coords = projectCam()
+
     for i in range(len(coords)):
         ground.data.vertices[i].co = coords[i]
